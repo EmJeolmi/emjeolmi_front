3add8d974eded67a79a017cf9c36194d62734b64
juseungeee <117347197+juseungeee@users.noreply.github.com>
2023-10-10 21:50:32 +0900

    Feat: landingpart 로그인 API

--- src/pages/LandingPage/landingpart.js
+++ src/pages/LandingPage/landingpart.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './landingpart.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { TitleText, Login } from '../../components/Login/login';
@@ -6,15 +7,36 @@ import { NoDiary } from '../../components/Screen1-p/screen1-p';
 import click1Img from '../../images/click2.png';
 import click2Img from '../../images/click1.png';
 import closepImg from '../../images/closep.png';
+import { authAPI } from '../../service/auth/authAPI.js';
 
 function LandingPart() {
+
+    const [formData, setFormData] = React.useState({
+        id:"",
+        passwd:"",
+    });
+
     const [NoDiaryOn, setNoDiaryOn] = useState(false); // 팝업
     const [isHovered, setIsHovered] = useState(false); // 클릭 버튼
+
+    const [error, setError] = useState(false); // 로그인 오류 상태
+    const [inputNullError, setInputNullError] = useState(false);
 
     const openNoDiary = () => { setNoDiaryOn(true); };
     const closeNoDiary = () => { setNoDiaryOn(false); };
     const onMouse = () => { setIsHovered(true); };
     const offMouse = () => { setIsHovered(false); };
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        try {
+            // 로그인 시 authAPI 호출
+            await authAPI(event, formData, setError, setInputNullError);
+        } catch (error) {
+            // 필요한 경우 오류 처리
+            console.error('로그인 중 오류 발생:', error);
+        }
+    };
 
     return (
         <>
@@ -27,13 +49,15 @@ function LandingPart() {
                         labelText="일기주소"
                         idText="일기주소를 입력해주세요 ~"
                         pwText="비밀번호를 입력해주세요 ~"
+                        formData={formData}
+                        setFormData={setFormData}
                     />
                 </div>
 
                 {/* 클릭 버튼 -- 일기 없을 때만 팝업 뜨도록 수정 */}
                 <div className='button-container'>
                     <button className='clickbtn1' 
-                        onClick={openNoDiary} 
+                        onClick={handleLogin} 
                         onMouseEnter={onMouse}
                         onMouseLeave={offMouse}
                     >
--- src/service/diary/checkMyDiaryAPI.js
+++ src/service/diary/checkMyDiaryAPI.js
@@ -19,10 +19,10 @@ export const checkMyDiaryAPI = async (event, accessToken) => {
         console.log('res.data.ok: ', res.data.ok);
         if (res.data.ok === true) {
             console.log('내 일기 읽어오기 성공');
-            window.location.href = "/savediary";
+            window.location.href = "../readdiary";
         } else {
-            console.log(    '내 일기 없음');
-            window.location.href = "/writediary";
+            console.log('내 일기 없음');
+            window.location.href = "../popupcheck";
         }
     } catch(error) {
         console.log('내 일기 읽어오기 실패'); 
